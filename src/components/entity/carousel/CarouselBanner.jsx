8c92f1497448e47c91d3a93cbd80ee03d56772ad
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2023-03-24 16:53:52 +0300

    carousel

--- src/components/entity/carousel/CarouselBanner.jsx
+++ src/components/entity/carousel/CarouselBanner.jsx
@@ -8,9 +8,12 @@ import {
   Button,
   getStylesRef,
   rem,
+  Grid,
 } from "@mantine/core";
-
+import { Carousel } from "@mantine/carousel";
 import { IconStar } from "@tabler/icons-react";
+import styles from "./CarouselBanner.module.scss";
+import Label from "@/components/shared/label/Label";
 
 const useStyles = createStyles((theme) => ({
   price: {
@@ -42,34 +45,108 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-const images = [
-  "https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-  "https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-  "https://images.unsplash.com/photo-1605774337664-7a846e9cdf17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-  "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-  "https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+const content = [
+  {
+    id: 1,
+    image_src:
+      "https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    title: "ОСЕНЬ / ЗИМА",
+    subtitle: "Коллекция",
+    buttonText: "Перейти",
+    buttonLink: "/",
+  },
+  {
+    id: 2,
+    image_src:
+      "https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    title: "Новое поступление",
+    subtitle: "Успей купить прямо сейчас",
+  },
+  {
+    id: 3,
+    image_src:
+      "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    title: "Скоро открытие",
+    subtitle: "Новый магазин в ТЦ Мытыщи",
+    buttonText: "Посмотреть на карте",
+    buttonLink: "/",
+  },
+  {
+    id: 4,
+    image_src:
+      "https://images.unsplash.com/photo-1605774337664-7a846e9cdf17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    title: "Финальная распродажа",
+    subtitle: "Купи со скидками до -50%",
+  },
+  {
+    id: 5,
+    image_src:
+      "https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    title: "Гарантия 2 года",
+    subtitle:
+      "На любую продукцию нашего веселого магазина, от производителя, длинный текст",
+    buttonText: "Перейти",
+    buttonLink: "/",
+    label: "green",
+  },
 ];
 
 export function CarouselBanner() {
   const { classes } = useStyles();
 
-  const slides = images.map((image) => (
-    <Carousel.Slide key={image}>
-      <Image src={image} height={220} />
+  const slides = content.map((item) => (
+    <Carousel.Slide key={item}>
+      <div className={styles.sliderContent}>
+        <Image src={item.image_src} height={420} className={styles.image} />
+        <div className={styles.content}>
+          <Grid>
+            <Grid.Col md={12} lg={6} align="center" justify="center">
+              <div className={styles.colContent}>
+                {item.label && <Label type={item.label}>Новика</Label>}
+                <Text
+                  component="span"
+                  variant="gradient"
+                  gradient={{ from: "#A6A9FF", to: "#6F73EE" }}
+                  inherit
+                  className={styles.title}
+                >
+                  {item.title}
+                </Text>
+                <Text className={styles.subtitle}>{item.subtitle}</Text>
+
+                {item.buttonText && (
+                  <Button
+                    variant="gradient"
+                    gradient={{ from: "#8185f9", to: "cyan" }}
+                  >
+                    {item.buttonText}
+                  </Button>
+                )}
+              </div>
+            </Grid.Col>
+            <Grid.Col md={12} lg={6}></Grid.Col>
+          </Grid>
+        </div>
+      </div>
     </Carousel.Slide>
   ));
 
   return (
-    <Carousel
-      withIndicators
-      loop
-      classNames={{
-        root: classes.carousel,
-        controls: classes.carouselControls,
-        indicator: classes.carouselIndicator,
-      }}
+    <div
+      style={{ marginBottom: "1rem", borderRadius: "16px", overflow: "hidden" }}
     >
-      {slides}
-    </Carousel>
+      <Carousel
+        withIndicators
+        loop
+        height={421}
+        classNames={{
+          root: classes.carousel,
+          controls: classes.carouselControls,
+          indicator: classes.carouselIndicator,
+        }}
+      >
+        {slides}
+      </Carousel>
+    </div>
   );
 }
