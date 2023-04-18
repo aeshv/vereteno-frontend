7154e014299e9d40fe79c7fb8ba901e52cbd29a5
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2023-04-18 16:40:52 +0300

    fix23

--- src/components/features/cart/NoDataCart/NoDataCart.jsx
+++ src/components/features/cart/NoDataCart/NoDataCart.jsx
@@ -3,19 +3,24 @@ import { IconMoodEmptyFilled } from "@tabler/icons-react";
 import Image from "next/image";
 import { Text, Title } from "@mantine/core";
 import styles from "./NoDataCart.module.scss";
+import BuyingWith from "../../product/blocks/BuyingWith/BuyingWith";
 
 const NoDataCart = () => {
   return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.image}>
-          <IconMoodEmptyFilled size="85px"/>
+    <>
+      <div>
+        <div className={styles.container}>
+          <div className={styles.image}>
+            <IconMoodEmptyFilled size="85px" />
+          </div>
+          <h2>ВАША КОРЗИНА ПУСТА</h2>
+          <p>Добавьте в нее товары из каталога</p>
+          <button className={styles.button}>Перейти в каталог</button>
         </div>
-        <h2>ВАША КОРЗИНА ПУСТА</h2>
-        <p>Добавьте в нее товары из каталога</p>
-        <button className={styles.button}>Перейти в каталог</button>
       </div>
-    </div>
+
+      <BuyingWith title={"Рекомендуем вам"} />
+    </>
   );
 };
 
