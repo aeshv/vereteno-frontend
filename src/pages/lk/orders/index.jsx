d2d7e413479469e3cf1297360d1d7ebc1ccfeaf6
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2023-05-10 15:12:08 +0300

    catalog button hover

--- src/pages/lk/orders/index.jsx
+++ src/pages/lk/orders/index.jsx
@@ -1,56 +1,64 @@
 import React from "react";
 import UserPageLayout from "@/components/Layouts/UserPageLayout";
-import {
-    Badge, Flex, Group, ScrollArea, Table, Text
-} from "@mantine/core";
-import {data} from "@/utils/mockdata";
-import {OrderHatPreview} from "@/components/widget/Order/OrderHatPreview";
+import { Badge, Flex, Group, ScrollArea, Table, Text } from "@mantine/core";
+import { data } from "@/utils/mockdata";
+import { OrderHatPreview } from "@/components/widget/Order/OrderHatPreview";
 
 const Index = () => {
-
-    return <>
-        <ScrollArea>
-            <Table miw={800} verticalSpacing="sm">
-                <thead>
-                <tr>
-                    <th>Заказ</th>
-                    <th>Дата</th>
-                    <th>Товары</th>
-                    <th>Статус</th>
-                </tr>
-                </thead>
-                {rows && (<tbody>{rows}</tbody>)}
-            </Table>
-        </ScrollArea>
-    </>;
+  return (
+    <>
+      <ScrollArea>
+        <Table miw={800} verticalSpacing="sm">
+          <thead>
+            <tr>
+              <th>Заказ</th>
+              <th>Дата</th>
+              <th>Товары</th>
+              <th>Стоимость</th>
+              <th>Статус</th>
+            </tr>
+          </thead>
+          {rows && <tbody>{rows}</tbody>}
+        </Table>
+      </ScrollArea>
+    </>
+  );
 };
 
-const rows = data.map((item) => (<tr key={item.name}>
+const rows = data.map((item) => (
+  <tr key={item?.name}>
     <td>
-        <Group spacing="sm">
-
-            <Flex direction={'row'} gap={'xs'} align={'center'}>
-                <Text fz="sm" c="dimmed">
-                    №
-                </Text>
-                <Text fz="sm" fw={500}>
-                    {item.orderNumber}
-                </Text>
-            </Flex>
-        </Group>
-    </td>
-    <td>{item.orderDate}</td>
-    <td>
-        <Flex style={{maxWidth: '250px'}} direction={'row'} gap={'xs'} align={'center'} wrap={'wrap'}>
-            {item.orderList.map((item, index) => (<OrderHatPreview key={index} {...item}/>))}
+      <Group spacing="sm">
+        <Flex direction={"row"} gap={"xs"} align={"center"}>
+          <Text fz="sm" c="dimmed">
+            №
+          </Text>
+          <Text fz="sm" fw={500}>
+            {item?.orderNumber}
+          </Text>
         </Flex>
+      </Group>
     </td>
+    <td>{item?.orderDate}</td>
     <td>
-        <Badge fullWidth>{item.status}</Badge>
+      <Flex
+        style={{ maxWidth: "250px" }}
+        direction={"row"}
+        gap={"xs"}
+        align={"center"}
+        wrap={"wrap"}
+      >
+        {item?.orderList.map((item, index) => (
+          <OrderHatPreview key={index} {...item} />
+        ))}
+      </Flex>
     </td>
-</tr>));
+    <td>{item?.orderPrice} ₽</td>
+    <td>
+      <Badge fullWidth>{item?.status}</Badge>
+    </td>
+  </tr>
+));
 
 Index.Layout = UserPageLayout;
 export default Index;
-
-
--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -1,43 +1,138 @@
 export const data = [
-    {
-        orderNumber: '1',
-        orderDate: '09-05-2023',
-        orderList: [
-            {name: 'Fedora White', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0015-400x400.jpg', id: 1}, {name: 'Шляпа', image_src: 'https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-1-400x400.jpg', id: 2}
-        ],
-        status: 'active'
-    },
-    {
-        orderNumber: '2323',
-        orderDate: '09-05-2023',
-        orderList: [{
-            name: 'Шляпа',
-            image_src: 'https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-2-400x400.jpg', id: 1}, {name: 'Шляпа', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 2}
-        ],
-        status: 'active'
-    },
-    {
-        orderNumber: '671',
-        orderDate: '09-05-2023',
-        orderList: [
-            {name: 'Шляпа большая', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 1}, {name: 'Шляпа', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 2},{name: 'Шляпа небольшая', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 3}, {name: 'Шляпа iks', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 4}
-        ],
-        status: 'active'
-    },
-    {
-        orderNumber: '526391',
-        orderDate: '09-05-2023',
-        orderList: [
-            {name: 'HatHat', image_src: 'https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-2-400x400.jpg', id: 1}, {name: 'Шляпа', image_src: 'https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-3-400x400.jpg', id: 2}
-        ],
-        status: 'active'
-    },
-    {
-        orderNumber: '123123434',
-        orderDate: '09-05-2023',
-        orderList: [
-            {name: 'Шляпа большая', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 1}, {name: 'Шляпа', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 2},{name: 'Шляпа небольшая', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 3}, {name: 'Шляпа iks', image_src: 'https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg', id: 4},{name: 'Федора маленькие поля', image_src: 'https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-2-400x400.jpg', id: 1}, {name: 'HatHat', image_src: 'https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-4-400x400.jpg', id: 2}
-        ],
-        status: 'deprecated'
-    }
-]
+  {
+    orderNumber: "1",
+    orderPrice: "1210",
+    orderDate: "09-05-2023",
+    orderList: [
+      {
+        name: "Fedora White",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0015-400x400.jpg",
+        id: 1,
+      },
+      {
+        name: "Шляпа",
+        image_src:
+          "https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-1-400x400.jpg",
+        id: 2,
+      },
+    ],
+    status: "active",
+  },
+  {
+    orderNumber: "2323",
+    orderPrice: "232220",
+    orderDate: "09-05-2023",
+    orderList: [
+      {
+        name: "Шляпа",
+        image_src:
+          "https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-2-400x400.jpg",
+        id: 1,
+      },
+      {
+        name: "Шляпа",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 2,
+      },
+    ],
+    status: "active",
+  },
+  {
+    orderNumber: "670",
+    orderPrice: "6001",
+    orderDate: "09-05-2023",
+    orderList: [
+      {
+        name: "Шляпа большая",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 1,
+      },
+      {
+        name: "Шляпа",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 2,
+      },
+      {
+        name: "Шляпа небольшая",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 3,
+      },
+      {
+        name: "Шляпа iks",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 4,
+      },
+    ],
+    status: "active",
+  },
+  {
+    orderNumber: "526391",
+    orderPrice: "5391",
+    orderDate: "09-05-2023",
+    orderList: [
+      {
+        name: "HatHat",
+        image_src:
+          "https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-2-400x400.jpg",
+        id: 1,
+      },
+      {
+        name: "Шляпа",
+        image_src:
+          "https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-3-400x400.jpg",
+        id: 2,
+      },
+    ],
+    status: "active",
+  },
+  {
+    orderNumber: "123123434",
+    orderPrice: "12340",
+    orderDate: "09-05-2023",
+    orderList: [
+      {
+        name: "Шляпа большая",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 1,
+      },
+      {
+        name: "Шляпа",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 2,
+      },
+      {
+        name: "Шляпа небольшая",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 3,
+      },
+      {
+        name: "Шляпа iks",
+        image_src:
+          "https://goggles.su/image/cache/catalog/hats/fedora/dsc_0006-400x400.jpg",
+        id: 4,
+      },
+      {
+        name: "Федора маленькие поля",
+        image_src:
+          "https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-2-400x400.jpg",
+        id: 1,
+      },
+      {
+        name: "HatHat",
+        image_src:
+          "https://goggles.su/image/cache/catalog/products/Fedora-Trilbi/Nova-Fedora-shlyapa-s-nizkoj-tulej-seraya-4-400x400.jpg",
+        id: 2,
+      },
+    ],
+    status: "deprecated",
+  },
+];
