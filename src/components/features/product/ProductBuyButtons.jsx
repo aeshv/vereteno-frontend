b6c5f8858b4f875f83ca01bbd87666d6ac5e267e
Alexey Vlasov <--global>
2023-05-25 20:51:45 +0300

    Buy button with section

---
+++ src/components/features/product/ProductBuyButtons.jsx
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import {Button, createStyles} from "@mantine/core";
+import {IconShoppingCart} from "@tabler/icons-react";
+import {useTimeout} from "@mantine/hooks";
+import {notifications} from "@mantine/notifications";
+
+const CatalogButtonStyles = createStyles(() => ({
+	button: {
+		background: "#6F73EE",
+		border: "1.5px solid #6F73EE",
+		borderRadius: "5px",
+		padding: "15px 45px",
+		width: '200px',
+		height: '53px',
+	},
+
+	buyContainer: {
+		marginTop: '1rem',
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+	}
+}));
+
+const ProductBuyButtons = () => {
+	const {classes} = CatalogButtonStyles();
+
+	const [isLoading, setIsLoading] = useState(false)
+
+	const handlePlaceToCart = () => {
+		setIsLoading((prevState) => !prevState)
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		setTimeout(() => handleSuccessAddToCard(), 1000);
+	}
+
+	const handleSuccessAddToCard = () => {
+		setIsLoading(false)
+		notifications.show({
+			title: "Успешно добавлено в корзину", message: 'Продолжите покупки или проверьте корзину', color: 'green'
+		})
+	}
+
+	const handleErrorAddToCard = () => {
+		setIsLoading(false)
+		notifications.show({
+			title: "Произошла ошибка", message: 'Попробуйте позже', color: 'red'
+		})
+	}
+
+	return (<div className={classes.buyContainer}>
+		<Button className={classes.button} loading={isLoading} leftIcon={<IconShoppingCart/>}
+						onClick={(e) => handlePlaceToCart(e)}>
+			В корзину
+		</Button>
+	</div>)
+}
+export default ProductBuyButtons
--- src/components/features/product/ProductOrder.jsx
+++ src/components/features/product/ProductOrder.jsx
@@ -1,38 +1,42 @@
-import { Text } from "@mantine/core";
 import React from "react";
 import styles from "./ProductOrder.module.scss";
-import { IconShare, IconScale, IconStack3 } from "@tabler/icons-react";
-const ProductOrder = () => {
-  return (
-    <>
-      <div className={styles.container}>
-        <h1 className={styles.product__title}>ЧЕРНАЯ ШЛЯПА ФЕДОРА BLIXEN EDITION С УЗКИМИ ПОЛЯМИ</h1>
-        <div className={styles.backinfo}>
-          <div className={styles.tag}>Купили: 129 раз</div>
-          <div className={styles.tag}>Артикул: ЫВАЫВА-020Ы</div>
-        </div>
-        <div className={styles.shopinfo}>
-          <div className={styles.shop__item}>
-            <IconStack3 size="1.05rem" stroke={1.5} />
-            <span>В наличии</span>
-          </div>
-          <div className={styles.shop__item}>
-            <IconScale size="1.05rem" stroke={1.5} />
-            <span>Сравнить</span>
-          </div>
-          <div className={styles.shop__item}>
-            <IconShare size="1.05rem" stroke={1.5} />
-            <span>Поделиться</span>
-          </div>
-        </div>
+import {IconShare, IconScale, IconStack3,} from "@tabler/icons-react";
+import ProductBuyButtons from "@/components/features/product/ProductBuyButtons";
 
-        <div className={styles.price}>
-          <span className={styles.old}>32 990 р.</span>
-          <span className={styles.current}>29 990 р.</span>
-        </div>
-      </div>
-    </>
-  );
+const ProductOrder = () => {
+
+
+	return (<>
+		<div className={styles.container}>
+			<h1 className={styles.product__title}>ЧЕРНАЯ ШЛЯПА ФЕДОРА BLIXEN EDITION С УЗКИМИ ПОЛЯМИ</h1>
+			<div className={styles.backinfo}>
+				<div className={styles.tag}>Купили: 129 раз</div>
+				<div className={styles.tag}>Артикул: ЫВАЫВА-020Ы</div>
+			</div>
+			<div className={styles.shopinfo}>
+				<div className={styles.shop__item}>
+					<IconStack3 size="1.05rem" stroke={1.5}/>
+					<span>В наличии</span>
+				</div>
+				<div className={styles.shop__item}>
+					<IconScale size="1.05rem" stroke={1.5}/>
+					<span>Сравнить</span>
+				</div>
+				<div className={styles.shop__item}>
+					<IconShare size="1.05rem" stroke={1.5}/>
+					<span>Поделиться</span>
+				</div>
+			</div>
+
+			<div className={styles.price}>
+				<span className={styles.old}>32 990 р.</span>
+				<span className={styles.current}>29 990 р.</span>
+			</div>
+
+			<ProductBuyButtons/>
+
+		</div>
+	</>);
 };
 
 export default ProductOrder;
