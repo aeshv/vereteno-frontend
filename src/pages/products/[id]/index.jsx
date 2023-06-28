22b45fce6c429608632977f23b6f4451b0d275c9
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2023-06-28 12:49:59 +0300

    product page removed

--- src/pages/products/[id]/index.jsx
+++ src/pages/products/[id]/index.jsx
@@ -7,57 +7,52 @@ import styles from "./SingleProduct.module.scss";
 import BuyingWith from "@/components/features/product/blocks/BuyingWith/BuyingWith";
 import {productApi} from "@/api";
 
-const SingleProduct = ({product}) => {
-	const {products: currentProduct} = product
-	console.log('ssssssssssssssssssssssssssss', currentProduct[0])
-	return (
-		<Paper shadow="xl" radius="xl" p="xl">
-			<div className={styles.container}>
-				<div className={styles.top}></div>
-				<div className={styles.body}>
-					<div className={styles.main}>
-						<ProductImage/>
-						<ProductOrder product={currentProduct[0]}/>
-					</div>
-					<div className={styles.additional}>
-						<ProductInfo product={product}/>
-						<BuyingWith title={"С этим товаром покупают"}/>
-						<BuyingWith title={"Похожие товары"}/>
-					</div>
-				</div>
-			</div>
-		</Paper>
-	);
+const SingleProduct = () => {
+    // const {products: currentProduct} = product
+    return (
+        <Paper shadow="xl" radius="xl" p="xl">
+            <div className={styles.container}>
+                <div className={styles.top}></div>
+                <div className={styles.body}>
+                    <div className={styles.main}>
+                        <ProductImage/>
+                        {/*<ProductOrder product={currentProduct[0]}/>*/}
+                    </div>
+                    <div className={styles.additional}>
+                        <ProductInfo product={product}/>
+                        <BuyingWith title={"С этим товаром покупают"}/>
+                        <BuyingWith title={"Похожие товары"}/>
+                    </div>
+                </div>
+            </div>
+        </Paper>
+    );
 };
 
 export default SingleProduct;
 
 
-export async function getStaticPaths() {
-
-	const {products} = await productApi.getProducts().then(({data}) => {
-		return data;
-	});
-
-	console.log('AAAAAAAAAAAAAAAAAAAAAAAA', products)
-
-	const paths = products.map((huy) => ({
-		params: {id: '' + huy.id, slug: huy.slug},
-	}))
-
-	console.log('------------------------------paths', paths)
-	console.log(typeof paths[0].params.id)
-	return {paths, fallback: false}
-}
-
-export const getStaticProps = async ({params}) => {
-	console.log('getStaticProps', params)
-	const product = await productApi.getProducts(params).then(({data}) => {
-		return data;
-	});
-	return {
-		props: {
-			product,
-		},
-	};
-}
+// export async function getStaticPaths() {
+//
+// 	const {products} = await productApi.getProducts().then(({data}) => {
+// 		return data;
+// 	});
+//
+// 	const paths = products.map((huy) => ({
+// 		params: {id: '' + huy.id, slug: huy.slug},
+// 	}))
+//
+// 	return {paths, fallback: false}
+// }
+//
+// export const getStaticProps = async ({params}) => {
+// 	console.log('getStaticProps', params)
+// 	const product = await productApi.getProducts(params).then(({data}) => {
+// 		return data;
+// 	});
+// 	return {
+// 		props: {
+// 			product,
+// 		},
+// 	};
+// }
