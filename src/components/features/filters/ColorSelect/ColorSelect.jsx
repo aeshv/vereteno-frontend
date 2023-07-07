2fabd486bb50afef45d7e7ad752e22550617cd3b
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2023-07-07 17:29:29 +0300

    colors multiselect

--- src/components/features/filters/ColorSelect/ColorSelect.jsx
+++ src/components/features/filters/ColorSelect/ColorSelect.jsx
@@ -5,100 +5,46 @@ import {
     Image,
     SimpleGrid,
     createStyles,
-    rem,
+    rem, MultiSelect,
 } from "@mantine/core";
 import {useUncontrolled} from "@mantine/hooks";
+import {useRouter} from "next/router";
+import React, {useState} from "react";
 
-const useStyles = createStyles((theme, {checked, color}) => ({
-    button: {
-        display: "flex",
-        alignItems: "center",
-        width: "100%",
-        transition: "background-color 150ms ease, border-color 150ms ease",
-        border: `${rem(1)} solid ${
-            checked
-                ? theme.fn.variant({variant: "outline", color: theme.primaryColor})
-                    .border
-                : theme.colorScheme === "dark"
-                    ? theme.colors.dark[8]
-                    : theme.colors.gray[3]
-        }`,
-        borderRadius: theme.radius.sm,
-        padding: theme.spacing.sm,
-        backgroundColor: checked
-            ? theme.fn.variant({variant: "light", color: theme.primaryColor})
-                .background
-            : theme.colorScheme === "dark"
-                ? theme.colors.dark[8]
-                : theme.white,
-    },
-
-    body: {
-        flex: 1,
-        marginLeft: theme.spacing.xs,
-    },
-    circle: {
-        width: 40,
-        height: 40,
-        background: color,
-    },
-}));
-
-export function ImageCheckbox({
-                                  checked,
-                                  defaultChecked,
-                                  onChange,
-                                  title,
-                                  description,
-                                  className,
-                                  color,
-                                  ...others
-                              }) {
-    const [value, handleChange] = useUncontrolled({
-        value: checked,
-        defaultValue: defaultChecked,
-        finalValue: false,
-        onChange,
-    });
-
-    const {classes, cx} = useStyles({checked: value, color: color});
-
-    return (
-        <UnstyledButton
-            {...others}
-            onClick={() => handleChange(!value)}
-            className={cx(classes.button, className)}
-        >
-            <div className={classes.circle}/>
-
-            <div className={classes.body}>
-                <Text weight={500} size="sm" sx={{lineHeight: 1}}>
-                    {title}
-                </Text>
-            </div>
-
-            <Checkbox
-                checked={value}
-                onChange={() => {
-                }}
-                tabIndex={-1}
-                styles={{
-                    input: {cursor: "pointer"},
-                    body: {marginLeft: "0.625rem"},
-                }}
-            />
-        </UnstyledButton>
-    );
-}
 
 const mockdata = [
-    {title: "Красный", color: "#dddd", id: 'cc-33'},
-    {title: "Бирюзовый", color: "#00cacc", id: 'cc-55'},
+    {label: "Красный", color: "#dddd", value: 'cc-33'},
+    {label: "Бирюзовый", color: "#00cacc", value: 'cc-55'},
 ];
 
 export function ColorSelect() {
-    const items = mockdata.map((item) => (
-        <ImageCheckbox {...item} key={item.title}/>
-    ));
-    return <SimpleGrid cols={1}>{items}</SimpleGrid>;
+
+    const router = useRouter()
+    const {query} = router
+    const [value, setValue] = useState(query?.color || []);
+
+
+    const onColorChange = (e) => {
+        if (e.length >= 1) {
+            //Значение цвета
+            setValue(e)
+            router.query.color = e
+            router.push(router)
+        } else {
+            setValue([])
+            if (router.query.color) {
+                delete router.query.color
+                router.push(router)
+            }
+        }
+
+    };
+
+    return (
+        <SimpleGrid cols={1}>
+            <MultiSelect value={value} onChange={onColorChange} data={mockdata} placeholder="Выберите цвет"
+                         searchable clearable
+                         nothingFound="Список пуст"/>
+        </SimpleGrid>
+    );
 }
