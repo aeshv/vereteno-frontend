19c3d94dd700e29ab152861b92a51a3edd06ce17
Alexey Vlasov <--global>
2023-08-22 21:03:35 +0300

    cleaning up, ver. changing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['goggles.su', 'images.unsplash.com', 'unsplash.com', '1467739-cx71348.tw1.ru'],
+    domains: ['goggles.su', 'images.unsplash.com', 'unsplash.com', '1467739-cx71348.tw1.ru', 'vereteno-hats.ru'],
   },
 }
 
