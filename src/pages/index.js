5642512709323964ef0bc3a13a2940f1bd7d40de
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2023-08-24 18:32:52 +0300

    favicons, clearing public assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import { useDispatch, useSelector } from "react-redux";
 import Card from "@/components/entity/card/Card";
 import { Flex, Grid, SimpleGrid } from "@mantine/core";
 import { CarouselBanner } from "@/components/entity/carousel/CarouselBanner";
@@ -7,12 +5,7 @@ import { FaqWithBg } from "@/components/entity/faq/FaqWithBg";
 import MainPageFeaturesContainer from "@/components/shared/features/MainPageFeaturesContainer/MainPageFeaturesContainer";
 import BuyingWith from "@/components/features/product/blocks/BuyingWith/BuyingWith";
 import BlurredBlock from "@/components/shared/BluredBlock/BlurredBlock";
-import { useEffect } from "react";
-import {
-  getCategories,
-  getProducts,
-} from "@/redux/features/products/productsSlice";
-import axios, { productApi } from "@/api";
+import { productApi } from "@/api";
 import { categoryApi } from "@/api/category";
 import PageHead from "@/components/SEO/PageHead";
 
