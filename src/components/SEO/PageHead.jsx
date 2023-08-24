380c9beb4c7b353d884bfa85a83762bfd30b22fd
Alexey Vlasov <--global>
2023-08-24 22:59:39 +0300

    yametrika

--- src/components/SEO/PageHead.jsx
+++ src/components/SEO/PageHead.jsx
@@ -1,17 +1,26 @@
 import Head from "next/head";
 import React from "react";
+import YandexMetrika from "@/utils/YandexMetrika";
 
 const PageHead = ({
-                      title = "Веретено, магазин головных уборов",
-                      description = "Купить шапки, шляпы, восьмиклинки быстро и просто",
-                  }) => {
-    return (
-        <Head>
-            <title>{title} - Веретено, магазин головных уборов</title>
-            <meta name="description" content={description}/>
-            <meta name="yandex-verification" content="8c42054833b5f11d" />
-        </Head>
-    );
+										title = "Веретено, магазин головных уборов",
+										description = "Купить шапки, шляпы, восьмиклинки быстро и просто",
+									}) => {
+	return (
+		<Head>
+			<title>{title} - Веретено, магазин головных уборов</title>
+			<meta name="description" content={description}/>
+			<meta name="yandex-verification" content="8c42054833b5f11d"/>
+			<YandexMetrika
+				yid={94732270}
+				clickmap={true}
+				trackLinks={true}
+				accurateTrackBounce={true}
+				webvisor={true}
+				ecommerce={"dataLayer"}
+			/>
+		</Head>
+	);
 };
 
 export default PageHead;
